ce0cc4c73fb1c2be969829845ba55d3365073e16
Junaid Parkar <100304984+JunaidParkar@users.noreply.github.com>
2023-05-16 23:21:37 +0530

    download CV updated

--- src/cv/createCV.jsx
+++ src/cv/createCV.jsx
@@ -1,12 +1,19 @@
-export const createCV = (e) => {
-    e.preventDefault()
-    let cv = "https://firebasestorage.googleapis.com/v0/b/junaidparkar-f7e41.appspot.com/o/Resume%20-%20Junaid%20Parkar.pdf?alt=media&token=8835a183-ed78-4ebc-91d9-26ada2e4157f"
-    let file = new Blob([cv], { type: 'application/pdf' })
-    let url = window.URL.createObjectURL(file)
-    let root = document.getElementById("root")
-    let anchor = document.createElement("a")
-    anchor.href = url
-    anchor.download = "Junaid Parkar Resume"
-    root.appendChild(anchor)
-    anchor.click()
-}
+export const createCV = async (e) => {
+  e.preventDefault();
+  const cvUrl = "https://firebasestorage.googleapis.com/v0/b/junaidparkar-f7e41.appspot.com/o/Resume%20-%20Junaid%20Parkar.pdf?alt=media&token=8835a183-ed78-4ebc-91d9-26ada2e4157f";
+  
+  try {
+    const response = await fetch(cvUrl);
+    const fileData = await response.blob();
+    const url = window.URL.createObjectURL(fileData);
+    const link = document.createElement("a");
+    link.href = url;
+    link.download = "Junaid Parkar Resume";
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+    window.URL.revokeObjectURL(url);
+  } catch (error) {
+    alert(error)
+  }
+};
